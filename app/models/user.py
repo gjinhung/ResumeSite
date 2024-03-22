538ebe666f7fcabb21e2ed704e290c0d525ac9dc
gjinhung <gjinhung2@gmail.com>
2024-03-21 23:56:17 -0400

    third

--- app/models/user.py
+++ app/models/user.py
@@ -17,8 +17,13 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(50), nullable=False)
     joined_on = db.Column(db.DateTime(), nullable=False)
     profile_pic = db.Column(db.String(255), nullable=True)
+    phone = db.Column(db.String(255), nullable=True)
+    location = db.Column(db.String(255), nullable=True)
     created_at = db.Column(db.DateTime(), nullable=False)
     updated_at = db.Column(db.DateTime(), nullable=False)
+
+    resumes = db.relationship("Resume", back_populates="user")
+    companies = db.relationship("Company", back_populates="user")
 
     @property
     def password(self):
@@ -40,9 +45,28 @@ class User(db.Model, UserMixin):
             "first_name": self.first_name,
             "joined_on": self.joined_on,
             "profile_pic": self.profile_pic,
+            "phone": self.phone,
+            "location": self.location,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
         }
+
+
+class Resume(db.Model):
+    __tablename__ = "sections"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False
+    )
+    created_at = db.Column(db.DateTime(), nullable=False)
+    updated_at = db.Column(db.DateTime(), nullable=False)
+
+    user = db.relationship("User", back_populates="resumes")
+    sections = db.relationship("Section", back_populates="resume")
 
 
 class Section(db.Model):
@@ -52,14 +76,82 @@ class Section(db.Model):
         __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(40), nullable=False, unique=True)
+    title = db.Column(db.String(40), nullable=True)
+    resume_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("resumes.id")), nullable=True
+    )
     created_at = db.Column(db.DateTime(), nullable=False)
     updated_at = db.Column(db.DateTime(), nullable=False)
+
+    resume = db.relationship("Resume", back_populates="sections")
+    companies = db.relationship("Company", back_populates="section")
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class Detail(db.Model):
+    __tablename__ = "details"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(40), nullable=False, unique=True)
+    company_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("companies.id")), nullable=False
+    )
+    created_at = db.Column(db.DateTime(), nullable=False)
+    updated_at = db.Column(db.DateTime(), nullable=False)
+
+    company = db.relationship("Company", back_populates="details")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class Company(db.Model):
+    __tablename__ = "companies"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    organization = db.Column(db.String(40), nullable=False, unique=True)
+    title = db.Column(db.String(40), nullable=False, unique=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False
+    )
+    section_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("sections.id")), nullable=False
+    )
+    start_date = db.Column(db.String(40), nullable=False, unique=True)
+    end_date = db.Column(db.String(40), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime(), nullable=False)
+    updated_at = db.Column(db.DateTime(), nullable=False)
+
+    section = db.relationship("Section", back_populates="companies")
+    details = db.relationship("Detail", back_populates="company")
+    user = db.relationship("User", back_populates="companies")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "organization": self.organization,
+            "title": self.title,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
         }
