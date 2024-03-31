8cd146631edcdb1a5b3dfce11cc21696801ff652
gjinhung <gjinhung2@gmail.com>
2024-03-30 22:21:15 -0400

    signup

--- app/forms/resume_form.py
+++ app/forms/resume_form.py
@@ -6,6 +6,4 @@ from app.models import User
 
 class ResumeForm(FlaskForm):
     user_id = IntegerField("tour_id")
-    date = StringField("date")
-    time = StringField("time")
     submit = SubmitField("Submit")
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, DateTimeField, BooleanField, DateField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from app.models import User
+
+
+ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+
+
+def valid_image_url(form, field):
+    file = field.data
+    if ("." in file and file.rsplit(".", 1)[1].lower() not in ALLOWED_EXTENSIONS) or (
+        "." not in file
+    ):
+        raise ValidationError("Invalid Image File Extension")
 
 
 def user_exists(form, field):
@@ -9,7 +21,7 @@ def user_exists(form, field):
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
-        raise ValidationError('Email address is already in use.')
+        raise ValidationError("Email address is already in use.")
 
 
 def username_exists(form, field):
@@ -17,11 +29,20 @@ def username_exists(form, field):
     username = field.data
     user = User.query.filter(User.username == username).first()
     if user:
-        raise ValidationError('Username is already in use.')
+        raise ValidationError("Username is already in use.")
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    username = StringField("username", validators=[DataRequired(), username_exists])
+    email = StringField("email", validators=[DataRequired(), user_exists])
+    password = StringField("password", validators=[DataRequired()])
+    first_name = StringField("first_name", validators=[DataRequired()])
+    last_name = StringField("last_name", validators=[DataRequired()])
+    joined_on = DateTimeField("joined_on")
+    profile_pic = FileField(
+        "profile_pic",
+        validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))],
+    )
+    phone = StringField("phone")
+    location = StringField("location")
+    submit = SubmitField("submit")
