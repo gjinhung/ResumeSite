f744eef61268faacd555c1b310e8fc1d19aec0b7
gjinhung <gjinhung2@gmail.com>
2024-05-09 23:19:20 -0400

    4

--- app/api/resume_routes.py
+++ app/api/resume_routes.py
@@ -3,6 +3,7 @@ from ..models import db, Resume, Section, Company, Detail
 from flask_login import login_required, current_user
 from ..forms.resume_form import ResumeForm
 import datetime
+from .auth_routes import validation_errors_to_error_messages
 
 resume_routes = Blueprint("resumes", __name__)
 
@@ -52,23 +53,23 @@ def get_user_resumes():
 def get_one_resume(id):
     resume = Resume.query.get(id)
     if resume:
-        resume_dict = {}
+        resume_dict = resume.to_dict()
         resume_dict["id"] = resume.id
         sections = resume.sections
         sections_list = []
         for section in sections:
             section_id = section.id
-            sect_dict = {}
+            sect_dict = section.to_dict()
             sect_dict["id"] = section.id
             companies = Company.query.filter_by(section_id=section_id)
             companies_list = []
             for company in companies:
-                company_dict = {}
+                company_dict = company.to_dict()
                 company_dict["id"] = company.id
                 details = Detail.query.filter_by(company_id=company.id)
                 details_list = []
                 for detail in details:
-                    detail_dict = {}
+                    detail_dict = detail.to_dict()
                     detail_dict["id"] = detail.id
                     tags = detail.tags
                     detail_dict["Tags"] = tags
@@ -92,6 +93,7 @@ def new_resume():
     form["csrf_token"].data = request.cookies["csrf_token"]
 
     if form.validate_on_submit():
+        form.title.data = form.title.data.strip()
         if not form.title.data:
             untitledResume = Resume.query.filter_by(title="Resume").count()
             if not untitledResume:
@@ -110,6 +112,21 @@ def new_resume():
 
                 form.title.data = f"Resume {resumeNum}"
 
+        else:
+            existingResume = Resume.query.filter_by(title=form.title.data).count()
+            if existingResume:
+                resumeNum = 1
+                untitledResume = Resume.query.filter_by(
+                    title=f"{form.title.data} {resumeNum}"
+                ).count()
+                while untitledResume:
+                    resumeNum = resumeNum + 1
+                    untitledResume = Resume.query.filter_by(
+                        title=f"{form.title.data}  {resumeNum}"
+                    ).count()
+
+                form.title.data = f"{form.title.data} {resumeNum}"
+
         resume = Resume(
             user_id=current_user.id,
             title=form.title.data,
@@ -125,6 +142,32 @@ def new_resume():
         return resume_dict
     else:
         return {"errors": "error in post a new Resume"}
+
+
+@resume_routes.route("/<int:id>", methods=["PUT"])
+@login_required
+def edit_resume(id):
+    form = ResumeForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    resume = Resume.query.get(id)
+    if current_user.id != resume.user_id:
+        return jsonify({"errors": "Unauthorized to edit this Resume"}), 403
+
+    if form.validate_on_submit():
+        form.title.data = form.title.data.strip()
+        if form.title.data:
+            existingResume = Resume.query.filter_by(title=form.title.data).count()
+            if existingResume:
+                return {"errors": f"{form.title.data} already exist"}
+            else:
+                resume.title = form.title.data
+                resume.updated_at = datetime.datetime.utcnow()
+
+        db.session.commit()
+
+        return resume.to_dict()
+    else:
+        return {"errors": validation_errors_to_error_messages(form.errors)}
 
 
 @resume_routes.route("/<int:id>/", methods=["DELETE"])
